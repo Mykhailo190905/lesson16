b22b632afc402410c8d8770b0e061b3c60f9da9e
Mykhailo190905 <mykhailobondarenko280@gmail.com>
2024-09-16 00:02:42 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 $(function () {
+  
   $("#tabs").tabs();
+
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    LazyLoad: 'ondemand',
+    swipeToSlide: true,
+    modileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false
+        }
+      } ,
+      {
+      breakpoint: 560,
+      settings: {
+        dots: false
+      }
+    }
+   
+    ]
+
+  });
 });
